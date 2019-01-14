24856fa40bdea30d616ebe0afef6f07298aca4a0
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-01-14 15:02:26 -0500

    Initial commit

---
+++ XYZ-Accelerometer/XYZ-Accelerometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XYZ-Accelerometer
+//
+//  Created by Thomas Donnelly on 1/14/19.
+//  Copyright © 2019 Thomas Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
